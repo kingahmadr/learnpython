a29e3735a86051f1e69632f8cbdff493c4a49780
kingahmadr <ahmadnromiz@gmail.com>
2024-09-26 10:36:38 +0700

    revised the inheritance method

--- oop/Class.py
+++ oop/Class.py
@@ -8,21 +8,25 @@ class Student:
         self.age = age
         self.raport = raport
 
+    def display_property(self):
+        print(f"Student details:\nnama: {self.name}\nusia: {self.age}\nnilai raport: {self.raport}")
+
 class School(Student):
-    total_student = int
+    total_student = {}
 
     def __init__(self, name, age, raport, total_student):
         super().__init__(name=name, age=age, raport=raport)
         self.total_student = total_student
 
     def display(self):
-        print(f"Student details:\nnama: {self.name}\nusia: {self.age}\nnilai raport: {self.raport}\nTotal Student: {self.total_student}")
+        super().display_property()
+        print(f"Total Student: {self.total_student}\n\n")
 
-
-school_object = School("Andi", 28, 29.5, 30)
-school_object.display()
-
-
+student_list = {}
+for i in range(5):
+    total_list_student = len(student_list)
+    student_list[i] = School("Murid " + str(i), 28+i, 29.5+i, total_list_student)
+    student_list[i].display()
 
 
         
